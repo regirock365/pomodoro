00104c93a0f409e59890c287bfa707fa65b5df80
Hemedi Saidi <hemedi@protonmail.com>
2018-01-06 13:39:12 +1030

    ...

--- public/main.js
+++ public/main.js
@@ -1,29 +1,45 @@
-const sessionLengthInput = document.getElementById("session-length")
-const breakLengthInput = document.getElementById("break-length")
-const circle = document.getElementById("circle")
+const DOM = {
+  session: document.getElementById("session-length"),
+  rest: document.getElementById("rest-length"),
+  circle: document.getElementById("circle"),
+  timeLeft: document.getElementById("time-left"),
+}
 
-let sessionLength = 25
-let breakLength = 5
-let endTime, interval, session = true
+let session = 25
+let rest = 5
+let endTime, interval, inSession = true
 
-sessionLengthInput.addEventListener("change", () => {
-  sessionLength = sessionLengthInput.value
-  console.log(sessionLength)
+DOM.session.addEventListener("change", () => {
+  session = DOM.session.value
+  console.log(session)
 })
-breakLengthInput.addEventListener("change", () => {
-  breakLength = breakLengthInput.value
-  console.log(breakLength)
+DOM.rest.addEventListener("change", () => {
+  rest = DOM.rest.value
+  console.log(rest)
 })
 
-circle.addEventListener("click", () => {
-  session = !session
-  const sessionInUse = (session ? breakLength : sessionLength)
+DOM.circle.addEventListener("click", () => {
+  // clear any previous interval
+  clearInterval(interval)
+
+  // toggle the session type & determine the session being currently used
+  inSession = !inSession
+  const sessionInUse = (inSession ? rest : session)
+
   endTime = Date.now() + 1000 * 60 * sessionInUse
-  interval = setInterval(function () {
-    console.log(endTime - Date.now())
-    if (Date.now() > endTime) {
-      alert("you are finished")
-      clearInterval(interval)
-    }
-  }, 1000)
+
+  intervalFunction()
+  interval = setInterval(intervalFunction, 1000)
 })
+
+// function to check the time remaining at set intervals
+const intervalFunction = () => {
+  const timeRemaining = Math.round((endTime - Date.now()) / 1000)
+  const minutesRemaining = Math.floor(timeRemaining / 60)
+  const secondsRemaining = timeRemaining % 60
+  console.log(minutesRemaining, secondsRemaining)
+  if (Date.now() > endTime) {
+    alert("you are finished")
+    clearInterval(interval)
+  }
+}
